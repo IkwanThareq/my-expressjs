b7827d2d0af13c68e64b1228d1c93981a14ecda3
Ikwan Thareq Kemal <ikwanthareq@gmail.com>
2023-05-18 18:30:19 +0700

    new route
    this push will create a new route about express js

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const app = express();
 
 app.get("/", function(req, res){
     res.send("<h1>hello, world</h1>");
-})
+});
+
+app.get("/contact", function(req, res){
+    res.send("Contact me at ikwanthareq@gmail.com");
+});
+
+app.get("/about", function(req, res){
+    res.send("my name is ikwan thareq k and the owner of this web!!");
+});
+
+// listen is the server listen on port 3000
 
 app.listen(3000, function(){
     console.log("server started in port 3000")
